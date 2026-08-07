d21034eea4c8a51eec83ef00922b80cc04f5cad9
Patrick762 <lutzpatti86@gmail.com>
2026-08-07 09:08:14 +0200

    Aktualisieren von getter.py

--- bluetti_modbus_lib/devices/getter.py
+++ bluetti_modbus_lib/devices/getter.py
@@ -3,7 +3,7 @@ from modbus_connection import ModbusUnit
 from .balco260 import Balco260
 
 
-def get_device(d: str, unit: ModbusUnit):
+def get_device(d: str, unit: ModbusUnit|None = None):
     if d == "balco260":
         return Balco260(unit)
     else:
