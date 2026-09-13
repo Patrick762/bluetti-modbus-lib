78827bbb217e656e6df91f45bd096484379f2599
Patrick762 <lutzpatti86@gmail.com>
2026-09-13 18:31:13 +0200

    make enum fields writable if enabled

--- bluetti_modbus_lib/fields/custom_fields.py
+++ bluetti_modbus_lib/fields/custom_fields.py
@@ -104,7 +104,7 @@ def field(
         case FieldType.STRING:
             reg = bluetti_string(address, length)
         case FieldType.ENUM:
-            reg = enum(address, enum_type, count=count, word_order="little")
+            reg = enum(address, enum_type, count=count, word_order="little", writable=writable)
 
     # handle extras
     if reg is not None:
